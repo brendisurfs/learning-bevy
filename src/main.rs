244f2dfe552758a74bd060ece26f5139a59852e2
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-04 13:36:05 -0700

    learned about startup systems
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Brendi".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Matt".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Grant".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn hello_world() {
-    println!("hello nerds!")
+    println!("hello world!");
 }
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
